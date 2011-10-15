8fc177f124ec5f9265ae3ab3cd531f749a8645f2
Jon Buckley <jon@jbuckley.ca>
2011-10-15 16:00:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+Input.js
+========
+Input.js is a JavaScript library to map controller and OS-specific USB enumerations provided by the [Gamepad API](https://wiki.mozilla.org/JoystickAPI) in [Mozilla Firefox](http://www.mozilla.org/firefox/) to an ideal virtual gamepad. While developed for use in [Paladin](https://wiki.mozilla.org/Paladin), Mozilla's open source 3D gaming initiative, Input.js is designed to be used by any web application.
+
+Info
+----
+* License: MIT (see included LICENSE file for full license)
+* Author: Jon Buckley [website](http://jbuckley.ca)
+* Github Repo: <https://github.com/jbuck/input.js>
+
+Supported Controllers
+---------------------
+* Xbox 360 Controller on Mac
+
+How to use Input.js in three steps
+----------------------------------
+* Load your copy of Input.js into your webpage:
+
+```
+<script type="text/javascript" src="input.js"></script>
+```
+
+* Create the Device object:
+
+```
+var gamepad;
+window.addEventListener("MozGamepadConnected", function(e) {
+  gamepad = new Input.Device(e.gamepad);
+});
+```
+
+* Use it:
+
+```
+if (gamepad.buttons.BUTTON_A) {
+  player.jump();
+}
+player.move(gamepad.buttons.LEFT_STICK_X, gamepad.buttons.LEFT_STICK_Y);
+```
+
+The API
+-------
+You should read the [Gamepad API](https://wiki.mozilla.org/JoystickAPI) as a background primer.
+There are 5 properties on the Input.Device object:
+
+* axes - An object that holds all of axis properties. They each return a value from -1.0 to +1.0.
+  * Left_Stick_X
+  * Left_Stick_Y
+  * Right_Stick_X
+  * Right_Stick_X
+* buttons - An object that holds all of the button properties. They each return a value from 0.0 to 1.0. Buttons can be digital or analog
+  * A_Button - Cross on the PS3
+  * B_Button - Circle on the PS3
+  * X_Button - Triangle on the PS3
+  * Y_Button - Square on the PS3
+  * Left_Stick_Button - L3 on the PS3
+  * Right_Stick_Button - R3 on the PS3
+  * Start_Button - 
+  * Back_Button - Select on the PS3
+  * Home_Button - Xbox or PS button in the middle of the controller
+  * Pad_Up - Up on the digital pad
+  * Pad_Down - Down on the digital pad
+  * Pad_Left - Left on the digital pad
+  * Pad_Right - Right on the digital pad
+  * Left_Trigger_1 - Left Bumper on the Xbox 360, L1 on the PS3
+  * Right_Trigger_1 - Right Bumper on the Xbox 360, R1 on the PS3
+  * Left_Trigger_2 - Left Trigger on the Xbox 360, L2 on the PS3
+  * Right_Trigger_2 - Right Trigger on the Xbox 360, R2 on the PS3
+* connected - A boolean that returns the current connected state of the device
+* id - A string with the USB vendor ID, USB device ID, and OS device name
+* index - A number starting at zero that represents the order the device was plugged in. Persists across reconnects.
---
+++ input.js
@@ -0,0 +1,163 @@
+/*
+Input.js is MIT-licensed software
+Copyright (c) 2011 Jon Buckley
+*/
+
+(function() {
+  // Holds all of the physical device to USB enumeration mappings
+  var keymapBlob = { 
+    '45e' : {
+      '28e' : {
+        'Mac' : {
+          'axes' : {
+            'Left_Stick_X': 0,
+            'Left_Stick_Y': 1,
+            'Right_Stick_X': 2,
+            'Right_Stick_Y': 3,
+            'Left_Trigger_2': 4,
+            'Right_Trigger_2': 5
+          },
+          'buttons' : {
+            'A_Button': 0,
+            'B_Button': 1,
+            'X_Button': 2,
+            'Y_Button': 3,
+            'Left_Trigger_1': 4,
+            'Right_Trigger_1': 5,
+            'Left_Stick_Button': 6,
+            'Right_Stick_Button': 7,
+            'Start_Button': 8,
+            'Back_Button': 9,
+            'Home_Button': 10,
+            'Pad_Up': 11,
+            'Pad_Down': 12,
+            'Pad_Left': 13,
+            'Pad_Right': 14
+          }
+        }
+      }
+    }
+  };
+
+  // Our ideal gamepad that we present to the developer
+  var ImaginaryGamepad = {
+    'axes' : [
+      'Left_Stick_X',
+      'Left_Stick_Y',
+      'Right_Stick_X',
+      'Right_Stick_Y'
+    ],
+    'buttons' : [
+      'A_Button',
+      'B_Button',
+      'X_Button',
+      'Y_Button',
+      'Left_Stick_Button',
+      'Right_Stick_Button',
+      'Start_Button',
+      'Back_Button',
+      'Home_Button',
+      'Pad_Up',
+      'Pad_Down',
+      'Pad_Left',
+      'Pad_Right',
+      'Left_Trigger_1',
+      'Right_Trigger_1',
+      'Left_Trigger_2',
+      'Right_Trigger_2'
+    ]
+  };
+
+  var osList = ['Win', 'Mac', 'Linux'];
+  function detectOS() {
+    for (var i in osList) {
+      if (navigator.platform.indexOf(osList[i]) !== -1) {
+        return osList[i];
+      }
+    }
+    return 'Unknown';
+  }
+
+  // Map imaginary device action to physical device action
+  function mapAxisToAxis(device, keymap, axes, prop) {      
+    Object.defineProperty(axes, prop, {
+      enumerable: true,
+      get: function() { return device.axes[keymap.axes[prop]]; }
+    });
+  }
+
+  function mapAxisToButton(device, keymap, axes, prop) {
+    Object.defineProperty(axes, prop, {
+      enumerable: true,
+      get: function() { return 0; }
+    });
+  }
+  
+  function mapButtonToButton(device, keymap, buttons, prop) {
+    Object.defineProperty(buttons, prop, {
+      enumerable: true,
+      get: function() { return device.buttons[keymap.buttons[prop]]; }
+    });
+  }
+
+  function mapButtonToAxis(device, keymap, buttons, prop) {
+    Object.defineProperty(buttons, prop, {
+      enumerable: true,
+      get: function() { return (device.axes[keymap.axes[prop]] + 1) / 2; }
+    });
+  }
+
+  var Input = window.Input = {};
+  var Device = Input.Device = function(domGamepad) {
+    if (!domGamepad) {
+      throw "You didn't pass a valid gamepad to the constructor";
+    }
+
+    var device = domGamepad,
+        usbVendor = domGamepad.id.split('-')[0],
+        usbDevice = domGamepad.id.split('-')[1],
+        os = detectOS(),
+        keymap = keymapBlob,
+        axes = this.axes = {},
+        buttons = this.buttons = {};
+
+    if (keymap && keymap[usbVendor] && keymap[usbVendor][usbDevice] && keymap[usbVendor][usbDevice][os]) {
+      keymap = keymap[usbVendor][usbDevice][os];
+    } else {
+      throw "A physical device layout for " + usbVendor + "-" + usbDevice + "-" + os + " isn't available";
+    }
+
+    // Wire the axes and buttons up
+    for (var a in ImaginaryGamepad.axes) {
+      if (keymap.axes[ImaginaryGamepad.axes[a]] !== undefined) {
+        mapAxisToAxis(device, keymap, axes, ImaginaryGamepad.axes[a]);
+      } else {
+        mapAxisToButton(device, keymap, axes, ImaginaryGamepad.axes[a]);
+      }
+    }
+
+    for (var b in ImaginaryGamepad.buttons) {
+      if (keymap.buttons[ImaginaryGamepad.buttons[b]] !== undefined) {
+        mapButtonToButton(device, keymap, buttons, ImaginaryGamepad.buttons[b]);
+      } else {
+        mapButtonToAxis(device, keymap, buttons, ImaginaryGamepad.buttons[b]);
+      }
+    }
+
+    // Add some useful properties from the DOMGamepad object
+    Object.defineProperty(this, "connected", {
+      enumerable: true,
+      get: function() { return device.connected; }
+    });
+
+    Object.defineProperty(this, "id", {
+      enumerable: true,
+      get: function() { return device.id; }
+    });
+
+    Object.defineProperty(this, "index", {
+      enumerable: true,
+      get: function() { return device.index; }
+    });
+  };
+}());
