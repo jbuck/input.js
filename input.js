68b48e9526257ae80a5065ed9618cec0231980a6
Raymond Hung <hung.raymond@gmail.com>
2011-10-19 20:41:53 -0400

    Added a controller to keymapBlob

--- input.js
+++ input.js
@@ -93,6 +93,36 @@ Copyright (c) 2011 Jon Buckley
           }
         }
       }
+    },
+    "46d": {
+      "c242": {
+        "Win": {
+          "axes": {
+            "Left_Stick_X": 0,
+            "Left_Stick_Y": 1,
+            "Right_Stick_Y": 4,
+            "Right_Stick_X": 3,
+            "Left_Trigger_2": 2,
+            "Right_Trigger_2": 2,
+            "Pad_Left": 5,
+            "Pad_Right": 5,
+            "Pad_Up": 6,
+            "Pad_Down": 6
+          },
+          "buttons": {
+            "A_Button": 0,
+            "X_Button": 2,
+            "B_Button": 1,
+            "Y_Button": 3,
+            "Left_Trigger_1": 4,
+            "Right_Trigger_1": 5,
+            "Back_Button": 6,
+            "Start_Button": 7,
+            "Left_Stick_Button": 8,
+            "Right_Stick_Button": 9
+          }
+        }
+      }
     }
   };
 
